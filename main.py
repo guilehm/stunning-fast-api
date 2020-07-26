c4834ee3ebb4696d8a5d49f7324b24a61684551c
Guilherme Luis <guile.hm@hotmail.com>
2020-07-26 12:01:46 -0300

    add typing and change to single quotes

--- main.py
+++ main.py
@@ -18,10 +18,10 @@ def read_root():
 
 
 @app.get('/items/{item_id}')
-def read_item(item_id, q=None):
+def read_item(item_id: int, q: Optional[str] = None):
     return {'item_id': item_id, 'q': q}
 
 
-@app.put("/items/{item_id}")
+@app.put('/items/{item_id}')
 def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
+    return {'item_name': item.name, 'item_id': item_id}
