556ccb3cc91ac46b7657e7795748f6534df4eaf4
Guilherme Luis <guile.hm@hotmail.com>
2020-07-26 11:50:04 -0300

    add items endpoint

--- main.py
+++ main.py
@@ -8,3 +8,8 @@ app = FastAPI()
 @app.get('/')
 def read_root():
     return {'Hello': 'World'}
+
+
+@app.get('/items/{item_id}')
+def read_item(item_id, q=None):
+    return {'item_id': item_id, 'q': q}
