d968b00a13d7f814dc73b78087b92afe2874df65
guilherme <guile.hm@hotmail.com>
2020-08-10 21:56:11 -0300

    create models file

--- models.py
+++ models.py
@@ -1,12 +1,16 @@
-from sqlalchemy import Boolean, Column, Integer, String
+from sqlalchemy import Column, Integer, String, Numeric
 
 from .database import Base
 
 
-class User(Base):
-    __tablename__ = "users"
+class Stock(Base):
+    __tablename__ = 'stocks'
 
     id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True, index=True)
-    hashed_password = Column(String)
-    is_active = Column(Boolean, default=True)
+    symbol = Column(String, unique=True, index=True)
+    price = Column(Numeric(10, 2))
+    forward_pe = Column(Numeric(10, 2))
+    forward_eps = Column(Numeric(10, 2))
+    dividend_yield = Column(Numeric(10, 2))
+    ma50 = Column(Numeric(10, 2))
+    ma200 = Column(Numeric(10, 2))
